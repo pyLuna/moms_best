2ccec1ea06c0ba51b8d698d0a2a5c2711c6c5a9c
pyLuna <john.joshua.demartin.celis@gmail.com>
2025-04-19 11:13:17 -0900

    initializing firebase

---
+++ server/src/firebase/init.ts
@@ -0,0 +1,17 @@
+import { App, initializeApp } from "firebase-admin/app";
+
+const initFirebase = (): App | null => {
+    const firebaseConfig = {
+        apiKey: process.env.APIKEY,
+        authDomain: process.env.AUTHDOMAIN,
+        projectId: process.env.PROJECTID,
+        storageBucket: process.env.STORAGEBUCKET,
+        messagingSenderId: process.env.MESSAGINGSENDERID,
+        appId: process.env.APPID,
+        measurementId: process.env.MEASUREMENTID
+    };
+
+    return initializeApp(firebaseConfig);
+}
+
+export default initFirebase;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import initFirebase from "./firebase/init";
+import { config } from "dotenv";
+import { PORT } from "./constants";
 
+// Load environment variables
+// Passing a `path` to config() will set the path for that environment file
+const configResult = config({ path: ".env.development" });
 const app = express();
+const firebaseAdminApp = initFirebase();
 
 app.get("/hello-world", (req, res) => {
     res.send({
@@ -8,6 +15,8 @@ app.get("/hello-world", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log("Server is running on port,", PORT);
+    console.log("Firebase initialized", firebaseAdminApp);
+    console.log("Environment variables", configResult);
 })
