806d967b612cf2e4e9f3733190042e0810b207b6
pyLuna <john.joshua.demartin.celis@gmail.com>
2025-07-27 13:14:05 +0800

    netlify update

--- server/src/netlify.ts
+++ server/src/netlify.ts
@@ -1,4 +1,11 @@
 import serverless from "serverless-http";
 import app from "./index";
 
-export const handler = serverless(app);
+// Add a catch-all route handler
+app.get("/.netlify/functions/netlify", (req, res) => {
+  res.json({ message: "Netlify function is working" });
+});
+
+export const handler = serverless(app, {
+  basePath: "/.netlify/functions/netlify",
+});
