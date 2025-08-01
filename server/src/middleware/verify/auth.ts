e8cf697935c966f4806182fd0167ae3a6ebf40ca
pyLuna <john.joshua.demartin.celis@gmail.com>
2025-08-02 00:04:26 +0800

    another log

--- server/src/middleware/verify/auth.ts
+++ server/src/middleware/verify/auth.ts
@@ -3,11 +3,12 @@ import { verifyToken } from "../../utils/tokens";
 
 const verifyAuthorization = (req: any, res: any, next: any) => {
   const sendUnauthorized = () =>
-    res.status(401).send({ error: "Unauthorized" });
+    res.status(401).send({ error: "Please log in." });
 
   const isSkippable = isSkip(req.path);
   if (isSkippable) return next();
 
+  console.log("Authorization Middleware: Checking token...", req.cookies.token);
   const isVerified = verifyToken(req.cookies.token);
   if (!isVerified) sendUnauthorized();
 
